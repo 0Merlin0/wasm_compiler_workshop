2c689fa78fca3b89285541d0d2631f7904436bf2
Julian P.M. Ertel <me@julian-ertel.de>
2023-02-01 00:59:58 +0100

    Iteration 1.5 - Implementation

--- src/generator.ts
+++ src/generator.ts
@@ -19,14 +19,47 @@ export class Generator {
             case 'program':
                 str += '(module \n';
                 str += '(func (export "_start") (result i32)\n';
-                str += this._generate(element.child) + '\n';
+                for (let child of element.children) {
+                    str += this._generate(child) + '\n';
+                }
                 str += '))\n';
                 break;
             case 'return':
-                str += 'i32.const ' + this._generate(element.child);
+                str += this._generate(element.child);
                 break;
-            case 'value':
-                str += element.value;
+            case 'sum':
+            case 'product':
+                if (element.children.length === 0) {
+                    str += this._generate(element.child);
+                } else {
+                    let tmp = this._generate(element.child);
+                    for (let child of element.children) {
+                        let sign = child[1];
+                        let operation = '(i32.';
+                        switch (sign) {
+                            case '+':
+                                operation += 'add'
+                                break;
+                            case '-':
+                                operation += 'sub'
+                                break;
+                            case '*':
+                                operation += 'mul'
+                                break;
+                            case '/':
+                                operation += 'div_s'
+                                break;
+                        }
+                        operation += ' ' + tmp + ' ';
+                        operation += this._generate(child[3]);
+                        operation += ')';
+                        tmp = operation;
+                    }
+                    str += tmp;
+                }
+                break;
+            case 'number':
+                str += '(i32.const ' + element.value + ')';
                 break;
         }
         return str;
